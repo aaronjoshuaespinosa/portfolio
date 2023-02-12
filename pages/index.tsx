c8cfa63b041324f456102d5274c620ef51df9e44
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-02-12 16:38:16 +0800

    Change meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,64 +4,114 @@ import { motion } from 'framer-motion';
 import { TbBrandGithub, TbBrandBehance, TbBrandDribbble, TbBrandLinkedin, TbBrandInstagram, TbBrandFacebook } from "react-icons/tb";
 
 export default function Home() {
+  const links = [
+    {
+      link: "#",
+      text: "Me",
+    },
+    {
+      link: "#",
+      text: "Projects",
+    },
+    {
+      link: "#",
+      text: "Contact",
+    },
+  ]
+
+  const icons = [
+    {
+      link: "https://github.com/eyrooonnn",
+      icon: <TbBrandGithub />,
+    },
+    {
+      link: "https://www.behance.net/aaronjoshuaespinosa",
+      icon: <TbBrandBehance />,
+    },
+    {
+      link: "https://dribbble.com/aaronjoshuaespinosa",
+      icon: <TbBrandDribbble />,
+    },
+    {
+      link: "https://www.linkedin.com/in/aaronjoshuaespinosa/",
+      icon: <TbBrandLinkedin />,
+    },
+    {
+      link: "https://www.instagram.com/_eyrooonnn/",
+      icon: <TbBrandInstagram />,
+    },
+    {
+      link: "https://www.facebook.com/eyrooonnn/",
+      icon: <TbBrandFacebook />,
+    },
+  ]
   return (
     <>
       <Head>
         <title>AJ Espinosa</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="http://www.coachesneedsocial.com/wp-content/uploads/2014/12/BannerWCircleImages-1.jpg" />
+        <meta property="og:description" content="Coaches share their secrets to success so you can rock 2015. Join us for this inspiring, rejuvenating, motivating look at what secret sauce these coaches use to succeed in their business. This is for coaches of any level that are committed to changing the world. You will be elevated both in your life and your coaching business. Check out the topics below, there is something for everyone." />
+        <meta property="og:url" content="http://www.coachesneedsocial.com/coacheswisdomtelesummit/" />
+        <meta property="og:title" content="Coaches Wisdom Telesummit" />
       </Head>
       <main className='w-full h-screen flex items-center justify-center bg-dark text-light font-space relative'>
-        <nav className='fixed top-0 flex flex-row justify-between items-center w-full p-[1rem] md:p-[1.5rem] lg:p-[3rem]'>
+        {/* NAV BAR */}
+        <nav className='fixed top-0 flex flex-row justify-between items-center w-full py-[2rem] px-[3.5rem] md:px-[4.5rem] lg:px-[7rem] lg:max-w-[100rem]'>
           <img className='h-[1.5rem]' src="https://ik.imagekit.io/xzgmktvzg/ajlogo?ik-sdk-version=javascript-1.4.3&updatedAt=1676132015789" />
-          <div className='flex-row gap-x-[0.75rem] text-accent text-[0.875] font-bold hover:text-accent hidden'>
-            <Link href="#"><p>nav.<span className='text-primary'>Me</span></p></Link>
-            <Link href="#"><p>nav.<span className='text-primary'>Projects</span></p></Link>
-            <Link href="#"><p>nav.<span className='text-primary'>Contact</span></p></Link>
+
+          {/* NAV LINKS */}
+          <div className='hidden flex-row gap-x-[1.5rem] text-accent text-[0.875] font-bold hover:text-accent'>
+            {links.map((link, i) => (
+              <Link href={link.link}><p>nav.<span className='text-primary'>{link.text}</span></p></Link>
+            ))}
           </div>
         </nav>
 
+        {/* LINKS */}
         <div className='flex flex-col text-[1.5rem] text-primary gap-y-[1.5rem] items-center absolute bottom-0 left-0 pl-[1rem] md:pl-[1.5rem] lg:pl-[3rem]'>
-          <Link href="https://github.com/eyrooonnn" target="blank">
-            <motion.p whileHover={{ y: -3, color: "#7B03FC" }} transition={{ duration: 0.2 }} className='cursor-pointer'><TbBrandGithub /></motion.p>
-          </Link>
+          {icons.map((icon, i) => (
+            <Link href={icon.link} target="blank">
+              <motion.p whileHover={{ y: -3, color: "#7B03FC" }} transition={{ duration: 0.2 }} className='cursor-pointer'>{icon.icon}</motion.p>
+            </Link>
+          ))}
+          <div className='w-[1px] h-[5rem] bg-primary' />
+        </div>
 
-          <Link href="https://www.behance.net/aaronjoshuaespinosa" target="blank">
-            <motion.p whileHover={{ y: -3, color: "#7B03FC" }} transition={{ duration: 0.2 }} className='cursor-pointer'><TbBrandBehance /></motion.p>
-          </Link>
-
-          <Link href="https://dribbble.com/aaronjoshuaespinosa" target="blank">
-            <motion.p whileHover={{ y: -3, color: "#7B03FC" }} transition={{ duration: 0.2 }} className='cursor-pointer'><TbBrandDribbble /></motion.p>
-          </Link>
-
-          <Link href="https://www.linkedin.com/in/aaronjoshuaespinosa/" target="blank">
-            <motion.p whileHover={{ y: -3, color: "#7B03FC" }} transition={{ duration: 0.2 }} className='cursor-pointer'><TbBrandLinkedin /></motion.p>
-          </Link>
-
-          <Link href="https://www.instagram.com/_eyrooonnn/" target="blank">
-            <motion.p whileHover={{ y: -3, color: "#7B03FC" }} transition={{ duration: 0.2 }} className='cursor-pointer'><TbBrandInstagram /></motion.p>
-          </Link>
-
-          <Link href="https://www.facebook.com/eyrooonnn/" target="blank">
-            <motion.p whileHover={{ y: -3, color: "#7B03FC" }} transition={{ duration: 0.2 }} className='cursor-pointer'><TbBrandFacebook /></motion.p>
+        <div className='flex flex-col text-[1.5rem] text-primary gap-y-[1.5rem] items-center justify-center absolute bottom-0 right-0 pr-[1rem] md:pr-[1.5rem] lg:pr-[3rem]'>
+          {/* EMAIL */}
+          <Link href="mailto:aaronjoshua.espinosa@yahoo.com" target="blank">
+            <motion.p whileHover={{ y: -3, color: "#7B03FC" }} transition={{ duration: 0.2 }} className='cursor-pointer text-vertical text-[0.875rem] leading-[1rem] tracking-[0.05rem]'>aaronjoshua.espinosa@yahoo.com</motion.p>
           </Link>
           <div className='w-[1px] h-[5rem] bg-primary' />
         </div>
 
+        {/* INFORMATION ABOUT ME */}
         <section className='flex flex-row justify-center items-center w-full px-[3.5rem] md:px-[4.5rem] lg:px-[7rem] lg:max-w-[100rem]'>
+
           <div className='w-full md:w-[50%] flex flex-col gap-y-[1.5rem]'>
+            {/* TITLE */}
             <p className='text-accent text-[0.9rem] lg:text-[1rem]'>Hello, my name is</p>
+
+            {/* NAME */}
             <div className='flex flex-col'>
               <h1 className='text-primary font-bold text-[2.5rem] md:text-[3rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3rem] lg:leading-[4rem]'>AJ Espinosa.</h1>
               <h1 className='text-secondary font-bold text-[2rem] md:text-[3rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3rem] lg:leading-[4rem]'>I am <span>a Developer.</span></h1>
             </div>
+
+            {/* DESCRIPTION */}
             <p className='text-secondary text-[0.9rem] lg:text-[1rem] mb-[2rem]'>A Frontend Web Developer and a Graphic Artist that loves his work. Currently, a 3rd Year Computer Science Student at Cavite State University - Don Severino Delas Alas Campus. Right now focused on learning more about web development using <Link href="https://nextjs.org/" target="blank"><span className='text-accent hover:underline'>NextJS</span></Link> and <Link href="https://tailwindcss.com/" target="blank"><span className='text-accent hover:underline'>Tailwind CSS</span></Link> and on improving my skills and creativity.</p>
+
+            {/* BUTTON */}
             <Link href="mailto:aaronjoshua.espinosa@yahoo.com" className='w-fit'><button className='w-fit text-accent border-[1px] border-accent rounded-[4px] text-[0.9rem] lg:text-[1rem] py-[1rem] px-[2rem] hover:bg-accent/10 transition-all ease-in-out duration-[0.2]'>Contact Me!</button></Link>
           </div>
 
+          {/* IMAGE */}
           <div className='hidden md:flex w-[50%] relative items-center justify-center'>
             <div className='bg-gradient bg-gradient-to-t from-dark to-transparent h-[10rem] w-full absolute bottom-0' />
             <img src="https://ik.imagekit.io/xzgmktvzg/aj_portfolio_image.png?ik-sdk-version=javascript-1.4.3&updatedAt=1676135167839" />
           </div>
+
         </section>
       </main>
     </>
